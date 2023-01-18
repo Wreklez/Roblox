2e57836f7cb7c608b5f2cda4ce0e49bbe4451ac8
Wreklez <kewin5322@gmail.com>
2023-01-18 03:38:59 +0000

    w

--- Script.lua
+++ Script.lua
@@ -1,4 +1,4 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Wreklez/Roblox/main/GUI.lua?token=GHSAT0AAAAAAB5TXE2T4WBHNVP5YAJ46REIY6HMLSA"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Wreklez/Roblox/main/GUI.lua?token=GHSAT0AAAAAAB5TXE2TBHMI7BP3SROERLIIY6HNHXA"))()
 local Window = Library.CreateLib("Wreklez Bee Swarm Simulator", "DarkTheme")
 
 if game.PlaceId == 1537690962 then
