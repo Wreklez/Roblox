edc3d90a1dfd44f6ec2323edf679cf722c63a6e3
Wreklez <97864134+Wreklez@users.noreply.github.com>
2023-01-18 02:41:04 +0000

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,10 +1,1 @@
-local Library = loadstring(game:HttpGet("https://https://raw.githubusercontent.com/Wreklez/Roblox/main/GUI.lua?token=GHSAT0AAAAAAB5TXE2SDIQXOQA3J23NTWLUY6HJFIQ"))()
-local Window = Library.CreateLib("Wreklez Bee Swarm Simulator", "DarkTheme")
 
-if game.PlaceId == 1537690962 then
-    
-    local Tab = Window:NewTab("Farm")
-    local Tab = Window:NewTab("Misc")
-    local Tab = Window:NewTab("Settings")
-
-end
